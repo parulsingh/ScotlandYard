5e6a0e62bd1ccce3399fc12a5ff1f8c287c0c422
parulsingh <parulsingh@berkeley.edu>
2017-04-18 00:19:47 -0700

    tabs working

--- src/App.js
+++ src/App.js
@@ -2,39 +2,41 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Chips from './Chips.js';
-import cab from './cab2.png';
-import tube from './tube.png';
-import bus from './bus.png';
 import {Tabs, Tab} from 'material-ui/Tabs';
 import FontIcon from 'material-ui/FontIcon';
 import MapsPersonPin from 'material-ui/svg-icons/maps/person-pin';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import injectTapEventPlugin from 'react-tap-event-plugin';
+
+injectTapEventPlugin();
 
 class App extends Component {
   render() {
     return (
+        <MuiThemeProvider>
+
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-        <Chips initialNum={11} imageString={cab} colorString={"#ffd700"}/>
-        <Chips initialNum={9} imageString={bus} colorString={"#009099"}/>
-        <Chips initialNum={4} imageString={tube} colorString={"#e72d2d"}/>
+        
          <Tabs>
           <Tab
-   
-          />
+            icon={<FontIcon className="material-icons">phone</FontIcon>}
+            label="RECENTS">
+             <Chips/>
+          </Tab>
           <Tab
-
-          />
+            icon={<FontIcon className="material-icons">favorite</FontIcon>}
+            label="FAVORITES">
+            <Chips/>
+            </Tab>
           <Tab
-
-          />
+            icon={<MapsPersonPin />}
+            label="NEARBY">
+            <Chips/>
+            </Tab>
         </Tabs>
       </div>
+        </MuiThemeProvider>
+
     );
   }
 }
--- src/Chips.js
+++ src/Chips.js
@@ -1,48 +1,15 @@
 import React, { Component } from 'react';
-import injectTapEventPlugin from 'react-tap-event-plugin';
-
-injectTapEventPlugin();
+import Chip from './Chip.js';
+import cab from './cab2.png';
+import tube from './tube.png';
+import bus from './bus.png';
 
 class Chips extends Component {
-  constructor(props){
-   super(props);
-   this.state={ count: props.initialNum}
-  }
-
-  handleClick = () => {
-    this.setState({
-         count: this.state.count - 1
-      });
-  }
-  
-
-
-
-  render() {
-    const buttonStyle = {
-      backgroundColor: this.props.colorString,
-      borderRadius: 15,
-      width: 82,
-      height: 50,
-      float: "left"
-    };
-
-    const divStyle = {
-      display:"inline-block"
-    };
-
-    const paragraphStyle = {
-      float: "right",
-      paddingLeft: 25
-    };
-    return (
-      <div>
-      <div style={divStyle}>
-        <button style={buttonStyle}> <img height="42" width="42" 
-        src={this.props.imageString} alt="my image" onClick={this.handleClick} /></button>
-        <h3 style={paragraphStyle}>x{this.state.count}</h3>
-      </div>
-      </div>);
-  }
+	render() {
+		<div className="App">
+		 <Chip initialNum={11} imageString={cab} colorString={"#ffd700"}/>
+              <Chip initialNum={9} imageString={bus} colorString={"#009099"}/>
+              <Chip initialNum={4} imageString={tube} colorString={"#e72d2d"}/>
+              </div>
+	}
 }
-export default Chips;
